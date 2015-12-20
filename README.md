8eb06a7455cd7019ff05b2d82c6ca07282b27b6e
Sasha Mile Rudan <mprinc@gmail.com>
2015-12-20 15:27:06 +0100

    installing jsdoc template Migrating documentation to docstrap template

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 If documentation is in ***qpp-web*** folder that is sibling with code folder ***qpp***, then the corrrect way to compile is:
 
-```
-index.js -c ../qpp-web/jsdoc/config.js -d ../qpp-web
+```bash
+jsdoc -c ../qpp-web/jsdoc/config.js 
 ```
--- jsdoc/config.js
+++ jsdoc/config.js
@@ -1,6 +1,38 @@
 {
+	"opts": {
+		"template": "./node_modules/ink-docstrap/template",  // same as -t templates/default
+		"encoding": "utf8",               // same as -e utf8
+		"destination": "../qpp-web",          // same as -d ./out/
+		"recurse": true                  // same as -r
+		// "tutorials": "path/to/tutorials", // same as -u path/to/tutorials
+	},
 	"plugins": ["plugins/markdown"],
 	"templates": {
-		"monospaceLinks": true
+		"monospaceLinks": true,
+		"systemName" : "QPP (Promises Augmentation & Patterns)",
+		"footer": "This work is contribution to nodejs open source community",
+		"copyright" :  "The MIT License (MIT)<br/>\nCopyright (c) 2015 Sasha Rudan",
+		"includeDate"           : "false",
+		"navType"   : "inline",
+		"theme"     : "cerulean",
+		"linenums"  : true,
+		"collapseSymbols"       : false,
+		"inverseNav": true,
+		"outputSourceFiles"     : true ,
+		"outputSourcePath"      : true,
+		"dateFormat": "dddd, MMMM Do YYYY", // http://momentjs.com/docs/#/displaying/format/
+		"syntaxTheme"           : "default", // https://github.com/tmont/sunlight/tree/master/src/themes
+		"sort"      : true
+	},
+	"markdown": {
+		"tags": ["params", "returns", "var", "vars", "type", "types"]
+	},
+	"source": {
+		"include": [
+			"./index.js"
+		],
+		"exclude": [ "../src/js/libs/jquery.js" ],
+		"includePattern": ".+\\.js(doc)?$",
+		"excludePattern": "(^|\\/|\\\\)_"
 	}
 }
